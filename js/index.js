cd2820ddb579b1156bc67745165624b39d8c3bdc
扬眉剑出鞘 <hjhlovewel@outlook.com>
2018-09-06 15:45:19 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -6,7 +6,8 @@
 
     $('html').css({
       fontSize: W / 750 * 100
-    })
+    });
+
   }
   reDisplay();
   var timer = null;
@@ -21,15 +22,20 @@
 
 
 $(function () {
+  var H = $(window).height();
+  $("#view").css({
+    height: H - ($('.nav').height() + $('.foot-list').height())
+  });
 
-  var num=0,timer=null;
+  var num = 0,
+    timer = null;
 
   function ban() {
     clearTimeout(timer);
     timer = setTimeout(() => {
       num++;
-      if(num>3){
-        num=0;
+      if (num > 3) {
+        num = 0;
       }
       $(".ban-img li").eq(num).addClass("active");
       $(".ban-img li").eq(num).siblings().removeClass('active');
@@ -41,5 +47,30 @@ $(function () {
 
   ban();
 
+  var scro = new iScroll("view",{
+    vScrollbar:false,
+    checkDOMChanges:true,
+    onScrollMove:function (e) {
+      if (this.y<-($(".banner").height())) {
+        $(".cb").css({
+          display:"block"
+        })
+      }else{
+        $(".cb").css({
+          display:"none"
+        })
+      }
+    },
+    onScrollEnd:function (e) {
+      if (this.y == 0) {
+        $(".cb").css({
+          display:"none"
+        })
+      }
+    }
+  });
 
+  $(".cb").click(function () {
+    scro.scrollTo(0,0,500);
+  })
 })
